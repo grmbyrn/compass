f7e9b59716155fc71c6555f46669178452a68696
Graeme <grmbrn89@gmail.com>
2026-04-24 14:16:05 +0200

    fix code rabbit suggestions

--- client/eslint.config.js
+++ client/eslint.config.js
@@ -4,10 +4,12 @@ import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
 import tsEslint from "@typescript-eslint/eslint-plugin";
 import tsParser from "@typescript-eslint/parser";
+import eslintPrettier from "eslint-config-prettier";
 
 export default [
   { ignores: ["dist"] },
   js.configs.recommended,
+  eslintPrettier,
   {
     files: ["**/*.{ts,tsx}"],
     plugins: {
@@ -27,6 +29,7 @@ export default [
         "warn",
         { allowConstantExport: true },
       ],
+      "no-unused-vars": "off",
       "@typescript-eslint/no-unused-vars": "warn",
       "@typescript-eslint/no-explicit-any": "warn",
     },
